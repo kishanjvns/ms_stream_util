b3c03ccae5c246f045afdb9f2b1758a609fa8aa8
kishanjvns <kishanjvns@gmail.com>
2024-01-27 10:13:45 +0530

    first commit

---
+++ src/main/java/com/tech/kj/JwtFilter.java
@@ -0,0 +1,94 @@
+package com.tech.kj;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    Logger log = LoggerFactory.getLogger(JwtFilter.class);
+    private final JwtTokenProvider jwtTokenUtil;
+    @Value("${jwt.header.string}")
+    public String HEADER_STRING;
+
+    @Value("${jwt.token.prefix}")
+    public String TOKEN_PREFIX;
+
+    @Value("${application.security.jwt.secret-key}")
+    private String SIGNING_KEY;
+
+    @Value("${jwt.authorities.key}")
+    public String AUTHORITIES_KEY;
+
+    public JwtFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenUtil = jwtTokenProvider;
+        //this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String header = request.getHeader(HEADER_STRING);
+        String username = null;
+        Claims claims = null;
+        String authToken = null;
+        if (header != null && header.startsWith(TOKEN_PREFIX)) {
+            authToken = header.replace(TOKEN_PREFIX, "").trim();
+            try {
+                username = jwtTokenUtil.getUsernameFromToken(authToken);
+                claims =jwtTokenUtil.getAllClaimsFromToken(authToken);
+
+                log.info("username:{} and claims: {} from token: {}",username,claims,authToken);
+            } catch (IllegalArgumentException e) {
+                log.error("An error occurred while fetching Username from Token", e);
+            } catch (ExpiredJwtException e) {
+                log.warn("The token has expired", e);
+            } catch (SignatureException e) {
+                log.error("Authentication Failed. Username or Password not valid.");
+            }
+        } else {
+            log.warn("Couldn't find bearer string, header will be ignored");
+        }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            final Collection<? extends GrantedAuthority> authorities =
+                    Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
+
+            UserDetails userDetails = new User(username, "", authorities);
+            if (jwtTokenUtil.isValidSignature(authToken, SIGNING_KEY)) {
+                UsernamePasswordAuthenticationToken authentication = jwtTokenUtil.getAuthenticationToken(authToken, SecurityContextHolder.getContext().getAuthentication(), userDetails);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                log.info("authenticated user " + username + ", setting security context");
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/tech/kj/JwtTokenProvider.java
@@ -0,0 +1,108 @@
+package com.tech.kj;
+
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+@Component
+public class JwtTokenProvider {
+    Logger log = LoggerFactory.getLogger(JwtTokenProvider.class);
+    private UserDetailsService userDetailsServiceImpl;
+
+    @Value("${application.security.jwt.secret-key}")
+    private String SIGNING_KEY;
+    @Value("${application.security.jwt.expiration}")
+    private long TOKEN_VALIDITY;
+
+    @Value("${jwt.authorities.key}")
+    public String AUTHORITIES_KEY;
+
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SIGNING_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(String authorities,String userName) {
+        log.info("generting token for userName: {} and its hashId: {}",userName,userName.hashCode());
+        log.info("generting token for authorities: {} and its hashId: {}",authorities,authorities.hashCode());
+        return Jwts.builder()
+                .setSubject(userName)
+                .claim(AUTHORITIES_KEY, authorities)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY*1000))
+                .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    UsernamePasswordAuthenticationToken getAuthenticationToken(final String token, final Authentication existingAuth, final UserDetails userDetails) {
+
+        final JwtParser jwtParser = Jwts.parser().setSigningKey(SIGNING_KEY);
+
+        final Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
+
+        final Claims claims = claimsJws.getBody();
+
+        final Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", authorities);
+    }
+
+    public  boolean isValidSignature(String authToken, String secretKey){
+        boolean isSignatureValid =false;
+        try {
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(authToken);
+
+            Claims claims = claimsJws.getBody();
+            isSignatureValid  =true;
+            return isSignatureValid;
+        }catch (SecurityException ex){
+            return isSignatureValid;
+        }
+    }
+}
